0f817298ae1afe5e8e60d9536c9d26c61f52ae16
Kiibaa <a_luigonhor@gmail.com>
2021-04-27 13:52:37 +0200

    bruh

---
+++ ExamenJS/validaciones/validaciones.js
@@ -0,0 +1,7 @@
+function validar(campo) {
+    if (campo.value == null || campo.value.length == 0) {        
+        mostrarError(campo.id, "Error: No contiene @");    
+    var str = "Email";
+    var n = str.indexOf("email");
+     n = @ }
+}
---
+++ ExamenJS3/funciones.js
@@ -0,0 +1,40 @@
+function calcularMayor() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+    var mensaje = "";
+    if (num1 > num2) {
+        mensaje = num1 + " es mayor que " + num2;
+    } else if (num2 > num1) {
+        mensaje = num2 + " es mayor que " + num1;
+    } else {
+        mensaje = num1 + " es igual que " + num2;
+    }
+    document.getElementById("resultadoMayor").innerHTML = mensaje;
+}
+
+function calcularMenor() {
+    var num3 = document.getElementById("num3").value;
+    var num4 = document.getElementById("num4").value;
+
+    var mensaje = "";
+    if (num3 < num4) {
+        mensaje = num3 + " es menor que " + num4;
+    } else if (num4 < num3) {
+        mensaje = num4 + " es menor que " + num3;
+    } else {
+        mensaje = num3 + " es igual que " + num4;
+    }
+    document.getElementById("resultadoMenor").innerHTML = mensaje;
+}
+
+function calculadora(operacion, numero5, numero6) {
+    alert(operacion);
+
+    var numero5 = document.getElementById("num5").value;
+    var numero6 = document.getElementById("num6").value;
+
+    var res = 0;
+    
+    if (operacion == 'suma') {
+        res = eval(numero5) + eval(numero6);
